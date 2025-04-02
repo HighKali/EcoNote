7b2f61876844166941a7a20850848bdfc33bfd69
High Kali <163762069+HighKali@users.noreply.github.com>
2025-04-02 21:36:27 +0200

    Update CryptoNoteBasic.h

--- src/CryptoNoteCore/CryptoNoteBasic.h
+++ src/CryptoNoteCore/CryptoNoteBasic.h
@@ -1,16 +1,116 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include <boost/utility/value_init.hpp>
-#include <CryptoNote.h>
+#include <vector>
+
+#include "Common/MemoryInputStream.h"
+#include "Common/StringTools.h"
+#include "Common/VectorOutputStream.h"
+#include "CryptoNoteConfig.h"
+#include "CryptoNoteSerialization.h"
+#include "Serialization/BinaryOutputStreamSerializer.h"
+#include "Serialization/BinaryInputStreamSerializer.h"
+#include "Serialization/CryptoNoteSerialization.h"
+#include "crypto/crypto.h"
+#include "crypto/hash.h"
 
 namespace CryptoNote {
-  const Crypto::Hash NULL_HASH = boost::value_initialized<Crypto::Hash>();
-  const Crypto::PublicKey NULL_PUBLIC_KEY = boost::value_initialized<Crypto::PublicKey>();
-  const Crypto::SecretKey NULL_SECRET_KEY = boost::value_initialized<Crypto::SecretKey>();
 
-  KeyPair generateKeyPair();
-}
+enum class TransactionRemoveReason : uint8_t {
+  INCLUDED_IN_BLOCK = 0,
+  TIMEOUT = 1
+};
+
+struct TransactionOutputToKeyDetails {
+  Crypto::PublicKey outputPublicKey;
+};
+
+struct TransactionOutputMultisignatureDetails {
+  std::vector<Crypto::PublicKey> keys;
+  uint32_t requiredSignatures;
+};
+
+struct TransactionOutputDetails {
+  uint64_t amount;
+  uint8_t type;
+  union {
+    TransactionOutputToKeyDetails toKey;
+    TransactionOutputMultisignatureDetails multisignature;
+  };
+};
+
+struct TransactionExtraDetails {
+  Crypto::PublicKey publicKey;
+  BinaryArray nonce;
+  BinaryArray raw;
+};
+
+struct TransactionDetails {
+  Crypto::Hash hash;
+  uint64_t size = 0;
+  uint64_t fee = 0;
+  uint64_t totalInputsAmount = 0;
+  uint64_t totalOutputsAmount = 0;
+  uint64_t mixin = 0;
+  uint64_t unlockTime = 0;
+  uint64_t timestamp = 0;
+  Crypto::Hash paymentId;
+  bool inBlockchain = false;
+  Crypto::Hash blockHash;
+  uint32_t blockHeight = 0;
+  TransactionExtraDetails extra;
+  std::vector<std::vector<Crypto::Signature>> signatures;
+  std::vector<uint32_t> inputKeyIndexes;
+  std::vector<TransactionOutputDetails> outputs;
+};
+
+struct BlockDetails {
+  uint8_t majorVersion = 0;
+  uint8_t minorVersion = 0;
+  uint64_t timestamp = 0;
+  Crypto::Hash prevBlockHash;
+  uint32_t nonce = 0;
+  bool isOrphaned = false;
+  uint32_t height = 0;
+  Crypto::Hash hash;
+  uint64_t difficulty = 0;
+  uint64_t reward = 0;
+  uint64_t baseReward = 0;
+  uint64_t blockSize = 0;
+  uint64_t transactionsCumulativeSize = 0;
+  uint64_t alreadyGeneratedCoins = 0;
+  uint64_t alreadyGeneratedTransactions = 0;
+  uint64_t sizeMedian = 0;
+  double penalty = 0.0;
+  uint64_t totalFeeAmount = 0;
+  std::vector<TransactionDetails> transactions;
+};
+
+struct AccountPublicAddress {
+  Crypto::PublicKey spendPublicKey;
+  Crypto::PublicKey viewPublicKey;
+  uint64_t thunderScore = 0; // Thunder Score inizializzato a 0
+};
+
+struct KeyPair {
+  Crypto::PublicKey publicKey;
+  Crypto::SecretKey secretKey;
+};
+
+} // namespace CryptoNote
